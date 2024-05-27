a8bb9fe80c5551f6346e459160b07485c8a495de
Zhousiru <i@zhousiru.com>
2024-05-27 22:14:26 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import generouted from '@generouted/react-router/plugin'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), generouted()],
+})
