935f61a90ae50da8f8dfe78ababaff2c20852c85
DawSon <3096567831@qq.com>
2024-05-30 13:51:17 +0800

    feat: Complete the initial rendering of the login and registration pages

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,56 @@
+import { Link } from '@/router'
+import { Button, Group, PasswordInput, TextInput } from '@mantine/core'
+import { useForm } from '@mantine/form'
+
 export default function Page() {
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      email: '',
+      password: '',
+      code: '',
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : '请输入正确的邮箱'),
+    },
+  })
+
   return (
-    <>
-      <div>登录</div>
-    </>
+    <div className="mx-auto flex h-screen w-full max-w-xs flex-col items-center justify-center">
+      <form
+        onSubmit={form.onSubmit((values) => console.log(values))}
+        className="fle  w-[30vw] min-w-[300px] rounded-md bg-white p-4 shadow-md"
+      >
+        <TextInput
+          label="邮箱"
+          placeholder="xxx@email.com"
+          key={form.key('email')}
+          {...form.getInputProps('email')}
+        />
+        <PasswordInput
+          mt="sm"
+          label="密码"
+          placeholder="Password"
+          key={form.key('password')}
+          {...form.getInputProps('password')}
+        />
+        <TextInput
+          mt="sm"
+          label="验证码"
+          key={form.key('code')}
+          {...form.getInputProps('code')}
+        />
+        <Link
+          to="/register"
+          className="mt-4  text-xs	  text-gray-400 hover:text-gray-700"
+        >
+          没有账号？前往注册
+        </Link>
+        <Group justify="flex-end" mt="md">
+          <Button type="submit">登录</Button>
+        </Group>
+      </form>
+    </div>
   )
 }
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,67 @@
+import {
+  Button,
+  Checkbox,
+  Group,
+  PasswordInput,
+  TextInput,
+} from '@mantine/core'
+import { useForm } from '@mantine/form'
+
 export default function Page() {
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value: string) =>
+        /^\S+@\S+$/.test(value) ? null : '请输入正确的邮箱',
+      confirmPassword: (value: string, values: string) =>
+        value !== values.password ? '请确认是否密码一致' : null,
+    },
+  })
+
   return (
-    <>
-      <div>注册</div>
-    </>
+    <div className="mx-auto flex h-screen w-full max-w-xs flex-col items-center justify-center">
+      <form
+        onSubmit={form.onSubmit((values) => console.log(values))}
+        className="w-[30vw]  min-w-[300px] rounded-md bg-white p-4 shadow-md"
+      >
+        <TextInput
+          label="邮箱"
+          placeholder="xxx@email.com"
+          key={form.key('email')}
+          {...form.getInputProps('email')}
+        />
+        <PasswordInput
+          mt="sm"
+          label="密码"
+          placeholder="Password"
+          key={form.key('password')}
+          {...form.getInputProps('password')}
+        />
+        <PasswordInput
+          mt="sm"
+          label="确认密码"
+          placeholder="Confirm password"
+          key={form.key('confirmPassword')}
+          {...form.getInputProps('confirmPassword')}
+        />
+        <Checkbox
+          mt="md"
+          label="阅读并接受《服务条款》和《隐私政策》"
+          size="12px"
+          key={form.key('termsOfService')}
+          {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+        />
+        <Group justify="flex-end" mt="md">
+          <Button type="submit">注册</Button>
+        </Group>
+      </form>
+    </div>
   )
 }
