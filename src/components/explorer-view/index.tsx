e130591947b76bc3134c46b0e760cef865fe3be2
Zhousiru <i@zhousiru.com>
2024-06-23 13:23:24 +0800

    feat: upload button for file explorer

--- src/components/explorer-view/index.tsx
+++ src/components/explorer-view/index.tsx
@@ -1,10 +1,10 @@
 import { cn } from '@/utils/cn'
-import { IconArrowUp } from '@tabler/icons-react'
+import { IconArrowUp, IconUpload } from '@tabler/icons-react'
 import { Item } from './item'
 
 export default function ExplorerView({ className }: { className?: string }) {
   return (
-    <div className={cn('flex flex-col', className)}>
+    <div className={cn('relative flex flex-col', className)}>
       <div className="box-border flex h-12 border-b">
         <button className="grid h-12 w-12 place-items-center border-r transition hover:bg-gray-200">
           <IconArrowUp stroke={1.5} />
@@ -30,6 +30,12 @@ export default function ExplorerView({ className }: { className?: string }) {
           ))}
         </div>
       </div>
+      <button
+        onClick={() => alert('上传文件')}
+        className="absolute bottom-4 right-8 grid h-12 w-12 place-items-center rounded-full bg-blue-500 text-white shadow-lg shadow-blue-200 transition hover:-translate-y-1"
+      >
+        <IconUpload size={20} />
+      </button>
     </div>
   )
 }
