f239bdec46199825de7bfe15ea955f07e9b3fde5
Zhousiru <i@zhousiru.com>
2024-06-27 14:54:00 +0800

    revert: create-share.tsx

--- src/components/modals/create-share.tsx
+++ src/components/modals/create-share.tsx
@@ -1,15 +1,15 @@
 import { $axios } from '@/api'
 import { ApiOk } from '@/api/types'
 import { notificationError } from '@/constants/notifications'
-import { Alert, Button, Modal, NativeSelect, TextInput } from '@mantine/core'
+import { Alert, Button, Modal, TextInput } from '@mantine/core'
 import { notifications } from '@mantine/notifications'
 import { IconCheck } from '@tabler/icons-react'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
 import { CopyGhostButton } from '../copy-ghost-button'
 
 export function CreateShareModal({
-  // path,
+  path,
   opened,
   onClose,
 }: {
@@ -19,27 +19,11 @@ export function CreateShareModal({
 }) {
   const [password, setPassword] = useState('')
   const [shareId, setShareId] = useState('')
-  const [bucketId, setBucketId] = useState('')
-  const [filePath, setFilePath] = useState('')
 
   const shareLink = window.location.origin + '/s/' + shareId
 
   // const queryClient = useQueryClient()
-  const { data } = useQuery({
-    queryKey: ['bucket-list'],
-    queryFn: () =>
-      $axios.post<
-        ApiOk<
-          Array<{
-            bucketId: number
-            bucketSpace: number
-            isPublic: string
-            foldPath: string
-            foldName: string
-          }>
-        >
-      >('/user/list-buckets'),
-  })
+
   useEffect(() => {
     if (!opened) return
     setPassword('')
@@ -54,8 +38,7 @@ export function CreateShareModal({
         {
           params: {
             password,
-            filePath,
-            bucketId,
+            filePath: path,
           },
         },
       ),
@@ -85,30 +68,11 @@ export function CreateShareModal({
           />
         </div>
       ) : (
-        <>
-          <NativeSelect
-            value={bucketId}
-            description="选择需要删除的共享链接"
-            onChange={(e) => setBucketId(e.target.value)}
-            data={data?.data?.data.map((item) => {
-              return {
-                value: item.bucketId.toString(),
-                label: item.foldName,
-              }
-            })}
-          />
-
-          <TextInput
-            label="存储路径"
-            value={filePath}
-            onChange={(e) => setFilePath(e.target.value)}
-          />
-          <TextInput
-            label="共享密码"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </>
+        <TextInput
+          label="共享密码"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       )}
 
       <div className="mt-4 flex justify-end">
