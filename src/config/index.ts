6b21cc20e2c9b8338ceb30ce08414ed38e0e917e
Zhousiru <i@zhousiru.com>
2024-06-29 22:36:45 +0800

    fix: share with no token

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,11 @@
 export const apiConfig = {
   baseUrl: 'https://entbase.dev.mofu.app/api',
-  protectedUrls: ['/file/', '/admin/', '/user/', '/share/'],
+  protectedUrls: [
+    '/file/',
+    '/admin/',
+    '/user/',
+    '/share/create',
+    '/share/list',
+    '/share/delete',
+  ],
 }
